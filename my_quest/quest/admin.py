e581ea48827e61b2a48d1fbf1df04cb75da51e91
Nazariy Malish <nmalish@glooni.com>
2025-03-13 11:18:34 +0200

    migration creating tables

--- my_quest/quest/admin.py
+++ my_quest/quest/admin.py
@@ -1,12 +1,6 @@
 from django.contrib import admin
 from .models import Question, Answer
 
-class AnswerInline(admin.TabularInline):
-    model = Answer
-    extra = 3
 
-class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('description', 'type')
-    inlines = [AnswerInline]
-
-admin.site.register(Question, QuestionAdmin)
+admin.site.register(Question)
+admin.site.register(Answer)
