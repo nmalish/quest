3d937cb3e000e6675ba7d7e581cb6b598090d7fd
Nazariy Malish <nmalish@glooni.com>
2025-03-28 19:52:41 +0200

    fix docker

--- my_quest/my_quest/settings.py
+++ my_quest/my_quest/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'quest' / 'static',
 ]
